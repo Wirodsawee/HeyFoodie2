41b038c1e0b7351e473dd0fb2641c3f57697d74b
GIFS <gifftty.giff@gmail.com>
2020-03-29 21:38:34 +0700

    install react

--- backend/HeyFoodie_api/HeyFoodie_api/settings.py
+++ backend/HeyFoodie_api/HeyFoodie_api/settings.py
@@ -37,10 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
     'HeyFoodie'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +58,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    # 'http://localhost:3000/'
 ]
 
 ROOT_URLCONF = 'HeyFoodie_api.urls'
